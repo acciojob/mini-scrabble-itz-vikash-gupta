9a035ffb152a990c79f4e6ea24a97c783009fc5e
itz-vikash-gupta <120251402+itz-vikash-gupta@users.noreply.github.com>
2023-01-08 12:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 //your code here
 function increment(){
-    let vk=document.getElementsByTagName('input')[0];
-    let v=vk.value;
+    let vk=document.getElementsByTagName('h3')[0];
+    let v=vk.innerHTML;
     ++v
-    vk.value=v;
+    vk.innerHTML=v;
     }
-function increment(){
-let vk=document.getElementsByTagName('h3')[0];
-let value=vk.innerHTML;
-++value
-vk.innerHTML=value;
-}
